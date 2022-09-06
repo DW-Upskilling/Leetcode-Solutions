a0def9ef64a61a4ed6a6464eaa03f5e12f3fa22b
Developers@Work <DevelopersWork@users.noreply.github.com>
2022-09-06 15:35:33 +0000

    streak: 37

---
+++ 814. Binary Tree Pruning/driver.py
@@ -0,0 +1,64 @@
+from Solution import Solution
+import json
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+if __name__ == '__main__':
+    data = []
+    with open('input', 'r') as fd:
+        data = "".join(fd.read().split('"')).split('\n')
+    output = []
+    for i in range(len(data)):
+        head = None
+        stack = []
+        child = 0
+        for i in data[i][1:-1].split(","):
+            if i == '': continue
+            if i == 'null': i = None
+            else: i = int(i)
+
+            if not len(stack):
+                head = TreeNode(i)
+                stack.append(head)
+            else:
+                node = stack[0]
+                n = TreeNode(i) if i != None else None
+                if child == 0:
+                    node.left = n
+                    if n: stack.append(n)
+                    child += 1
+                else:
+                    child = 0
+                    node.right = n
+                    if n: stack.append(n)
+                    stack.pop(0)
+        head = Solution().pruneTree(head)
+
+        stack = [head]
+        _output = []
+        while len(stack):
+            _s = []
+
+            for n in stack:
+                if n:
+                    _output.append(n.val)
+                else:
+                    _output.append(None)
+                    continue
+                
+                if not n.left and not n.right:
+                    continue
+
+                stack.append(n.left)
+                stack.append(n.right)
+            
+            stack = _s
+
+        output.append(str(_output))
+    with open('output', 'w') as fd:
+        fd.write("\n".join(output))
+        fd.close()
